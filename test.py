4ea6f8e3c36c311750d16ef5ac9c981109e5ed85
Sean Hellum <seanhellum45@gmail.com>
2019-12-04 18:59:59 +0000

    test get_all_equations more

--- test.py
+++ test.py
@@ -11,3 +11,6 @@ def test_factor():
 def test_get_all_equations():
     assert main.get_all_equations(" rgieghoegneon -2x^2-4x-3 ergneiongeion") == ["-2x^2-4x-3"]
     assert main.get_all_equations("2x^3 rggrege 2x^2") == ["2x^2"]
+    assert main.get_all_equations("2x^2-5x \n") == ["2x^2-5x"]
+    assert main.get_all_equations("(x-3)^2-5") == ["(x-3)^2-5"]
+    assert main.get_all_equations("(x+2)^2") == ["(x+2)^2"]
