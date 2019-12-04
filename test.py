847061c7828144780f46269104e5e0e15034d91e
Sean Hellum <seanhellum45@gmail.com>
2019-12-04 19:02:10 +0000

    format

--- test.py
+++ test.py
@@ -8,9 +8,12 @@ def test_factor():
     assert main.factor(2, 6, 5) == (1, 5)
     assert main.factor(6, -5, -4) == (-8, 3)
 
+
 def test_get_all_equations():
-    assert main.get_all_equations(" rgieghoegneon -2x^2-4x-3 ergneiongeion") == ["-2x^2-4x-3"]
+    assert main.get_all_equations(
+        " rgieghoegneon -2x^2-4x-3 ergneiongeion") == ["-2x^2-4x-3"]
     assert main.get_all_equations("2x^3 rggrege 2x^2") == ["2x^2"]
-    assert main.get_all_equations("2x^2-5x \n 2x^2-5x+3") == ["2x^2-5x","2x^2-5x+3"]
+    assert main.get_all_equations(
+        "2x^2-5x \n 2x^2-5x+3") == ["2x^2-5x", "2x^2-5x+3"]
     assert main.get_all_equations("(x-3)^2-5") == ["(x-3)^2-5"]
     assert main.get_all_equations("(x+2)^2") == ["(x+2)^2"]
